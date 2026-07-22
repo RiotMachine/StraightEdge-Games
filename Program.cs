c14c64c0a9e024b7280ddd60aa8cb5b4fedd8a87
Jacob J. <jirak87@outlook.com>
2026-07-22 16:42:29 -0500

    Building polymorphism

--- Program.cs
+++ Program.cs
@@ -3,17 +3,37 @@ using System;
 namespace Games
 {
     public class Program
-    {                
+    {
+        public enum Option
+        {
+            Hangman,
+            HiLo
+        }
+
+        public static Game InitGame(Option o)
+        
+
+        private static Option ChooseGame()
+        {
+            for (int
+            
+            return
+            {
+
+
+            }
+        }
+
         public static void Play(Game game)
         {
             Console.WriteLine($"Welcome to {game.Name}.\n");
             game.Play();
             game.PrintResult();
-        }
+        }    
         
         public static void Main(string[] args)
         {
-            Play(new Hangman());
+            Play(ChooseGame());
         }
     }
 }
